db00e672d8a71a2715a541dde46a9aa9530e2f57
wangjian <wangjian@dayang.com.cn>
2025-07-25 13:45:27 +0800

    添加使用OpenGL绘制的三维空间的带双纹理矩形的CoordinateSystems工程

---
+++ CoordinateSystems/src/CoordinateWidget.h
@@ -0,0 +1,37 @@
+#pragma once
+#ifndef COORDINATEWIDGET_H
+#define COORDINATEWIDGET_H
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_3_3_Core>
+#include <QKeyEvent>
+
+class CoordinateWidget : public QOpenGLWidget, public QOpenGLFunctions_3_3_Core
+{
+    Q_OBJECT
+public:
+    explicit CoordinateWidget(QWidget* parent = nullptr);
+    virtual ~CoordinateWidget();
+
+protected:
+    virtual void initializeGL() override;
+    virtual void resizeGL(int width, int height) override;
+    virtual void paintGL() override;
+
+    virtual void keyReleaseEvent(QKeyEvent* event) override;
+
+private:
+    void initShaderProgram();
+    void initObject();
+    void initTexture(unsigned& texture, const QString& strImgFile, int nImgType);
+
+    QByteArray readShaderFile(const QString& strShaderFile);
+    void checkCompileLinkStatus(unsigned& shader, int nShaderType = -1);
+
+    unsigned VAO, VBO, EBO;
+    unsigned m_nShaderProgram;
+    unsigned m_nTexture1, m_nTexture2;
+};
+
+#endif  // COORDINATEWIDGET_H
+
